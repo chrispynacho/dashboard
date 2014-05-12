5c88f0606d446b1e1bfd46d0f9fab22134b02497
DTrejo <david.daniel.trejo@gmail.com>
2014-05-12 13:29:46 -0700

    progress bar and general css

--- neodashboard/packages/default/widgets/utilization/utilization.js
+++ neodashboard/packages/default/widgets/utilization/utilization.js
@@ -1,8 +1,7 @@
 widget = {
     //runs when we receive data from the job
     onData: function(el, data) {
-		data.utilization = 60;
-
-        $('.content', el).html(data.utilization);
+    data.utilization = 60
+    $('.content', el).css({ width: data.utilization + '%' });
     }
 };
