3b9916b59ccde43d1c33dd387558b73250ad10e8
Chris Camacho <chrispynacho@yahoo.com>
2014-05-12 16:27:27 -0700

    CC calendar tweaks

--- neodashboard/packages/demo/widgets/image/image.js
+++ neodashboard/packages/demo/widgets/image/image.js
@@ -6,7 +6,7 @@ widget = {
         }
 		
 		var imgTag = "<img class='featured-image' src='" + data.imageSrc + "'/>";
-		//data.caption.replace('#NEOINNOVATE', '');
+		//var captionText = data.caption.replace('#NEOINNOVATE', '') || caption.text;
 		var caption = '<div class="caption">' + data.caption.text + '</div> <div class="user">- @' + data.caption.from.username + '</div>';
 		
         $('.content', el).html(imgTag + caption);
