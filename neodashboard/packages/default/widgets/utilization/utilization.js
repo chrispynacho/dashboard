847630047dedef1572b93144c3b01c8594cccf14
DTrejo <david.daniel.trejo@gmail.com>
2014-05-12 14:04:25 -0700

    better utilization styling

--- neodashboard/packages/default/widgets/utilization/utilization.js
+++ neodashboard/packages/default/widgets/utilization/utilization.js
@@ -1,7 +1,7 @@
 widget = {
     //runs when we receive data from the job
-    onData: function(el, data) {
+  onData: function(el, data) {
     data.utilization = 60
     $('.content', el).css({ width: data.utilization + '%' });
-    }
+  }
 };
